c6c43f375cb1b8963460a57dae97fdd3de690a2e
Mori <35340393+Irom1@users.noreply.github.com>
2022-12-14 18:30:28 +0000

    push

--- Strings.java
+++ Strings.java
@@ -1,6 +1,7 @@
 public class Strings {
     // ACSL STRINGS
-    // 1. char_split (a$, n, ‘char’) – char_split divides a given string (a$) into substrings of length n separated by
+    // 1. char_split (a$, n, ‘char’) – char_split divides a given string (a$) into
+    // substrings of length n separated by
     public static String char_split(String a$, int n, char c) {
         String result = "";
         for (int i = 0; i < a$.length(); i++) {
@@ -11,6 +12,7 @@ public class Strings {
         }
         return result;
     }
+
     // 2. strrem (a$, b$) – removes all occurrences of b$
     public static String strrem(String a$, String b$) {
         String result = "";
@@ -27,7 +29,9 @@ public class Strings {
         }
         return result;
     }
-    // 3. strchr (a$, b$) – strchr searches a$ for the first occurrence of b$ and returns the characters of a$ before the first occurrence of b$
+
+    // 3. strchr (a$, b$) – strchr searches a$ for the first occurrence of b$ and
+    // returns the characters of a$ before the first occurrence of b$
     public static String strchr(String a$, String b$) {
         String result = "";
         for (int i = 0; i < a$.length(); i++) {
@@ -43,24 +47,22 @@ public class Strings {
         }
         return result;
     }
-    // 4. strtok(a$, b$) – strtok splits a given string (a$) into substrings each time the character b$ is found. Substrings must be separated by spaces.
+
+    // 4. strtok(a$, b$) – strtok splits a given string (a$) into substrings each
+    // time the character b$ is found. Substrings must be separated by spaces.
     public static String strtok(String a$, String b$) {
         String result = "";
         for (int i = 0; i < a$.length(); i++) {
-            if (i + b$.length() <= a$.length()) {
-                if (a$.substring(i, i + b$.length()).equals(b$)) {
-                    result += " ";
-                    i += b$.length() - 1;
-                } else {
-                    result += a$.charAt(i);
-                }
-            } else {
-                result += a$.charAt(i);
-            }
+            if (a$.charAt(i) == b$.charAt(0))
+                result += " ";
+            result += a$.charAt(i);
         }
         return result;
     }
-    // 5. wordwrap(a$, n, b$) – wordwrap outputs the substrings of a$ that are a maximum of n characters in length or that begin with the character b$. Substrings must be separated by spaces.
+
+    // 5. wordwrap(a$, n, b$) – wordwrap outputs the substrings of a$ that are a
+    // maximum of n characters in length or that begin with the character b$.
+    // Substrings must be separated by spaces.
     public static String wordwrap(String a$, int n, String b$) {
         String result = "";
         int count = 0;
