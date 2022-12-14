19e7d79c9207006afd459fafb1852c599ff6310d
Mori <35340393+Irom1@users.noreply.github.com>
2022-12-14 20:08:15 +0000

    complete

--- Main.java
+++ Main.java
@@ -2,20 +2,32 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    // get input for scanner
-    Scanner input = new Scanner(System.in);
-    String string1 = input.nextLine();
-    String string2 = input.nextLine();
-    String string3 = input.nextLine();
-    String string4 = input.nextLine();
-    String string5 = input.nextLine();
-    input.close();
-    // input defualt values for testing
-    string1 = "MISSISSIPPI, 2, *";
-    string2 = "MISSISSIPPI, IS";
-    string3 = "MISSISSIPPI, P";
-    string4 = "MISSISSIPPI, I";
-    string5 = "MISSISSIPPI, 2, I";
+    boolean testing = false;
+    String string1, string2, string3, string4, string5;
+    if (!testing) {
+      // get input for scanner
+      Scanner input = new Scanner(System.in);
+      string1 = input.nextLine();
+      string2 = input.nextLine();
+      string3 = input.nextLine();
+      string4 = input.nextLine();
+      string5 = input.nextLine();
+      input.close();
+    } else {
+      // input defualt values for testing
+      string1 = "MISSISSIPPI, 2, *";
+      string2 = "MISSISSIPPI, IS";
+      string3 = "MISSISSIPPI, P";
+      string4 = "MISSISSIPPI, I";
+      string5 = "MISSISSIPPI, 2, I";
+      /*
+MISSISSIPPI, 2, *
+MISSISSIPPI, IS
+MISSISSIPPI, P
+MISSISSIPPI, I
+MISSISSIPPI, 2, I
+       */
+    }
     // parse "MISSISSIPPI, 2, *" into char_split("MISSISSIPPI", 2, '*')
     String[] split = string1.split(", ");
     String a$ = split[0];
@@ -32,12 +44,12 @@ public class Main {
     a$ = split[0];
     b$ = split[1];
     System.out.println(Strings.strchr(a$, b$));
-    // MISSISSIPPI, I
+    // parse "MISSISSIPPI, I" into strtok("MISSISSIPPI", "I")
     split = string4.split(", ");
     a$ = split[0];
     b$ = split[1];
     System.out.println(Strings.strtok(a$, b$));
-    // MISSISSIPPI, 2, I
+    // parse "MISSISSIPPI, 2, I" into wordwrap("MISSISSIPPI", 2, "I")
     split = string5.split(", ");
     a$ = split[0];
     n = Integer.parseInt(split[1]);
--- Strings.java
+++ Strings.java
@@ -6,6 +6,7 @@ public class Strings {
         String result = "";
         for (int i = 0; i < a$.length(); i++) {
             if (i % n == 0 && i != 0) {
+                // inbetween substrings
                 result += c;
             }
             result += a$.charAt(i);
@@ -16,15 +17,13 @@ public class Strings {
     // 2. strrem (a$, b$) – removes all occurrences of b$
     public static String strrem(String a$, String b$) {
         String result = "";
-        for (int i = 0; i < a$.length(); i++) {
-            if (i + b$.length() <= a$.length()) {
-                if (a$.substring(i, i + b$.length()).equals(b$)) {
-                    i += b$.length() - 1;
-                } else {
-                    result += a$.charAt(i);
-                }
+        int i = 0;
+        while (i < a$.length()) {
+            if (a$.substring(i).indexOf(b$) == 0) {
+                i += b$.length();
             } else {
                 result += a$.charAt(i);
+                i++;
             }
         }
         return result;
@@ -33,19 +32,12 @@ public class Strings {
     // 3. strchr (a$, b$) – strchr searches a$ for the first occurrence of b$ and
     // returns the characters of a$ before the first occurrence of b$
     public static String strchr(String a$, String b$) {
-        String result = "";
-        for (int i = 0; i < a$.length(); i++) {
-            if (i + b$.length() <= a$.length()) {
-                if (a$.substring(i, i + b$.length()).equals(b$)) {
-                    return result;
-                } else {
-                    result += a$.charAt(i);
-                }
-            } else {
-                result += a$.charAt(i);
-            }
+        int index = a$.indexOf(b$);
+        if (index != -1) {
+            return a$.substring(0, index);
+        } else {
+            return a$;
         }
-        return result;
     }
 
     // 4. strtok(a$, b$) – strtok splits a given string (a$) into substrings each
@@ -54,7 +46,7 @@ public class Strings {
         String result = "";
         for (int i = 0; i < a$.length(); i++) {
             if (a$.charAt(i) == b$.charAt(0))
-                result += " ";
+                result += " "; // reached b$ so add space
             result += a$.charAt(i);
         }
         return result;
@@ -67,16 +59,14 @@ public class Strings {
         String result = "";
         int count = 0;
         for (int i = 0; i < a$.length(); i++) {
-            if (count == n) {
+            if (count == n || a$.charAt(i) == b$.charAt(0)) {
+                // reached max length or reached b$
                 result += " ";
-                count = 0;
-            }
-            if (a$.charAt(i) == b$.charAt(0)) {
-                result += " ";
-                count = 0;
+                count = 1;
+            } else {
+                count++;
             }
             result += a$.charAt(i);
-            count++;
         }
         return result;
     }
