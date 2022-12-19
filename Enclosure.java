7ce0f7cb1fa718d4bebbce0a4ceed829466a05af
Mori <35340393+Irom1@users.noreply.github.com>
2022-12-19 02:22:16 +0000

    changes

--- Enclosure.java
+++ Enclosure.java
@@ -1,6 +1,8 @@
 public class Enclosure {
     private String cleanedString;
     private char addingChar;
+    private int startEnclosureIndex;
+    private int endEnclosureIndex;
 
     // create a constructor
     public Enclosure(String string) {
@@ -10,19 +12,26 @@ public class Enclosure {
 
     // create a method to run the program
     public void run() {
+        boolean addCharToLeftSide = true;
         System.out.println();
         // find the point where the string is unbalanced
         int unbalancedIndex = findUnbalancedIndex();
-        System.out.println(cleanedString);
-        System.out.println(unbalancedIndex);
-        System.out.println(addingChar);
+        System.out.println("Initial string: " + cleanedString);
+        //System.out.println(unbalancedIndex);
+        System.out.println("Char to be added: " + addingChar);
         // get the part of the string before the unbalanced point
         String newPart = cleanedString.substring(0, unbalancedIndex);
         if(addingChar == ')' || addingChar == ']') {
             newPart = cleanedString.substring(unbalancedIndex);
+            addCharToLeftSide = false;
         }
-        System.out.println(newPart);
+        System.out.println("Should be added to " + (addCharToLeftSide ? "left" : "right") + " side");
+        //System.out.println(newPart);
         // find places to add the char
+        String inside = findInside(newPart);
+        System.out.println("Can be added in this segment: " + inside);
+        System.out.println("Range start: " + startEnclosureIndex);
+        System.out.println("Range end: " + endEnclosureIndex);
         //
     }
 
@@ -50,10 +59,43 @@ public class Enclosure {
         }
         if (unbalancedIndex == -1) {
             unbalancedIndex = cleanedString.lastIndexOf(openChars);
+            startEnclosureIndex = unbalancedIndex;
+            endEnclosureIndex = cleanedString.length() - 1;
+        } else {
+            endEnclosureIndex = unbalancedIndex;
+            startEnclosureIndex = 0;
         }
         char opener = cleanedString.charAt(unbalancedIndex);
         addingChar = reverseChar(opener);
         return unbalancedIndex;
+    }
+
+    private String findInside(String outside) {
+        String inside = "";
+        inside = clean(outside, "(", true);
+        inside = clean(inside, ")", false);
+        inside = clean(inside, "[", true);
+        inside = clean(inside, "]", false);
+        // find indexes of first and last char of inside
+        int firstIndex = outside.indexOf(inside);
+        if(firstIndex == -1) System.out.println("firstIndex is -1");
+        startEnclosureIndex += firstIndex;
+        endEnclosureIndex = startEnclosureIndex + inside.length() - 1;
+        return inside;
+    }
+    private String clean(String outside, String seperator, boolean rightSide) {
+        String inside = "";
+        int i = outside.indexOf(seperator);
+        if (i != -1) {
+            if (rightSide) {
+                inside = outside.substring(i + 1);
+            } else {
+                inside = outside.substring(0, i);
+            }
+        } else {
+            inside = outside;
+        }
+        return inside;
     }
 
     private char reverseChar(char c) {
--- Main.java
+++ Main.java
@@ -15,11 +15,11 @@ public class Main {
       input.close();
     } else {
       // input defualt values for testing
-      string1 = "[ 2 + 3 * 8 - 3 ) ] + 6";
-      string2 = "[ ( 2 - 5 ) + 6";
-      string3 = "[ ( 5 + 5 - 2 ] * 5";
-      string4 = "13 - [ ( 6 + 18 ) / 3 * 22";
-      string5 = "[ 4 / ( 12 - 8 / 4 * 25 ]";
+      string1 = "[ 2 + 3 * 8 - 3 ) ] + 6"; // 2, 4, 6
+      string2 = "[ ( 2 - 5 ) + 6"; // 7, 9
+      string3 = "[ ( 5 + 5 - 2 ] * 5"; // 6, 8
+      string4 = "13 - [ ( 6 + 18 ) / 3 * 22"; // 11, 13, 16
+      string5 = "[ 4 / ( 12 - 8 / 4 * 25 ]"; // 9, 11, 14
       /*
 [ 2 + 3 * 8 - 3 ) ] + 6
 [ ( 2 - 5 ) + 6
